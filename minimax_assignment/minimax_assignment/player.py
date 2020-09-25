82b1ddc56d31dfd3a4030a614bb96bc9655d7e97
Xuecong Liu <47290005+xc-liu@users.noreply.github.com>
2020-09-25 19:00:40 +0200

    fixed timeout error

--- minimax_assignment/minimax_assignment/player.py
+++ minimax_assignment/minimax_assignment/player.py
@@ -4,12 +4,13 @@ import random
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
+import time
+
+global start_time
 
 
-def shortest_distance(pos1, pos2):
-    dist1 = ((pos1[0] - pos2[0]) ** 2 + (pos1[1] - pos2[1]) ** 2) ** 0.5
-    dist2 = ((pos1[0] - pos2[0] - 20) ** 2 + (pos1[1] - pos2[1]) ** 2) ** 0.5
-    return min(dist1, dist2)
+def shortest_distance_squared(pos1, pos2):
+    return min((pos1[0] - pos2[0]) ** 2, (pos1[0] - pos2[0] - 20) ** 2) + (pos1[1] - pos2[1]) ** 2
 
 
 class PlayerControllerHuman(PlayerController):
@@ -80,29 +81,39 @@ class PlayerControllerMinimax(PlayerController):
         return None
 
     def alphabeta(self, node, depth, alpha, beta):
-        if depth == 0 or not node.compute_and_get_children():
-            fish_pos = node.state.get_fish_positions()
-            fish_score = node.state.get_fish_scores()
-            hook_pos = node.state.get_hook_positions()
-            fish = node.state.get_fish_positions().keys()
-            scoreA = sum([fish_score[f] / (0.05 + shortest_distance(hook_pos[0], fish_pos[f])) ** 2 for f in fish])
-            scoreB = sum([fish_score[f] / (0.05 + shortest_distance(hook_pos[1], fish_pos[f])) ** 2 for f in fish])
-            return scoreA - scoreB
 
-        elif node.state.get_player() == 0:
-            v = -1e6
-            for child in node.compute_and_get_children():
-                v = max(v, self.alphabeta(child, depth - 1, alpha, beta))
-                alpha = max(alpha, v)
-                if beta <= alpha:
-                    break
+        children = node.compute_and_get_children()
+        state = node.state
+
+        global start_time
+        if depth == 0 or (not children) or (time.time() - start_time > 0.064):
+            fish_pos = state.get_fish_positions()
+            fish_score = state.get_fish_scores()
+            hook_pos = state.get_hook_positions()
+
+            score_diff = 0
+            for f in fish_pos.keys():
+                score_diff += (fish_score[f] / (1 + shortest_distance_squared(hook_pos[0], fish_pos[f]))
+                               - fish_score[f] / (1 + shortest_distance_squared(hook_pos[1], fish_pos[f])))
+
+            scores = state.get_player_scores()
+            return score_diff + scores[0] - scores[1]
+
         else:
-            v = 1e6
-            for child in node.compute_and_get_children():
-                v = min(v, self.alphabeta(child, depth - 1, alpha, beta))
-                beta = min(beta, v)
-                if beta <= alpha:
-                    break
+            if state.get_player() == 0:
+                v = -1e6
+                for child in children:
+                    v = max(v, self.alphabeta(child, depth - 1, alpha, beta))
+                    alpha = max(alpha, v)
+                    if beta <= alpha:
+                        break
+            else:
+                v = 1e6
+                for child in children:
+                    v = min(v, self.alphabeta(child, depth - 1, alpha, beta))
+                    beta = min(beta, v)
+                    if beta <= alpha:
+                        break
         return v
 
     def search_best_next_move(self, model, initial_tree_node):
@@ -121,18 +132,13 @@ class PlayerControllerMinimax(PlayerController):
 
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
+
         child_nodes = initial_tree_node.compute_and_get_children()
-        child_v = [self.alphabeta(child, 2, -1e6, 1e6) for child in child_nodes]
+        child_v = []
+        global start_time
+        for child in child_nodes:
+            start_time = time.time()
+            child_v.append(self.alphabeta(child, 2, -1e6, 1e6))
 
-        new_pos = (child_nodes[child_v.index(max(child_v))]).state.get_hook_positions()[0]
-        curr_pos = initial_tree_node.state.get_hook_positions()[0]
-        bestMove = (new_pos[0] - curr_pos[0], new_pos[1] - curr_pos[1])
-
-        MOVE_TO_STR = {
-            (0, 0): "stay",
-            (0, 1): "up",
-            (0, -1): "down",
-            (-1, 0): "left",
-            (1, 0): "right"
-        }
-        return MOVE_TO_STR[bestMove]
+        bestMove = (child_nodes[child_v.index(max(child_v))]).move
+        return ACTION_TO_STR[bestMove]
