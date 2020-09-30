89f01b45938bbaa20c5dae13fe943b2b67b9444e
Xuecong Liu <47290005+xc-liu@users.noreply.github.com>
2020-09-30 18:55:06 +0200

    Update player.py

--- minimax_assignment/minimax_assignment/player.py
+++ minimax_assignment/minimax_assignment/player.py
@@ -1,16 +1,33 @@
 #!/usr/bin/env python3
-import random
-
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
+import random
 import time
 
 global start_time
+global heuristic_tree
 
 
 def shortest_distance_squared(pos1, pos2):
     return min((pos1[0] - pos2[0]) ** 2, (pos1[0] - pos2[0] - 20) ** 2) + (pos1[1] - pos2[1]) ** 2
+
+
+def computeHash(fish_pos, hook_pos, ZobristTable):
+    h = 0
+    for fish in fish_pos:
+        id = fish + 1
+        h ^= ZobristTable[fish_pos[fish][0]][fish_pos[fish][1]][id]
+    for hook in hook_pos:
+        h ^= ZobristTable[hook_pos[hook][0]][hook_pos[hook][1]][hook]
+    return h
+
+
+def addHookHash(hook_pos, prev_hash, ZobristTable):
+    h = prev_hash
+    for hook in hook_pos:
+        h ^= ZobristTable[hook_pos[hook][0]][hook_pos[hook][1]][hook]
+    return h
 
 
 class PlayerControllerHuman(PlayerController):
@@ -77,44 +94,81 @@ class PlayerControllerMinimax(PlayerController):
 
         Please note that the number of fishes and their types is not fixed between test cases.
         """
+        # Initializes hash table
+        ZobristTable = [[[None for _ in range(len(initial_data) + 1)] for _ in range(20)] for _ in range(20)]
+        for i in range(len(ZobristTable)):
+            for j in range(len(ZobristTable[0])):
+                for k in range(len(ZobristTable[0][0])):
+                    ZobristTable[i][j][k] = random.randint(0, 3000)
         # EDIT THIS METHOD TO RETURN A MINIMAX MODEL ###
-        return None
+        return ZobristTable
 
-    def alphabeta(self, node, depth, alpha, beta):
-
-        children = node.compute_and_get_children()
-        state = node.state
+    def alphabeta(self, ZobristTable, node, depth, alpha, beta, prevChildHash=None):
 
         global start_time
+        state = node.state
+        children = node.compute_and_get_children()
         if depth == 0 or (not children) or (time.time() - start_time > 0.064):
+
+            global heuristic_tree
+            if prevChildHash:
+                hashValue = addHookHash(node.state.get_hook_positions(), prevChildHash, ZobristTable)
+            else:
+                hashValue = computeHash(state.get_fish_positions(), state.get_hook_positions(), ZobristTable)
+
+            if hashValue in heuristic_tree:
+                return [heuristic_tree[hashValue], hashValue]
+
             fish_pos = state.get_fish_positions()
             fish_score = state.get_fish_scores()
             hook_pos = state.get_hook_positions()
+            curr_scores = state.get_player_scores()
 
-            score_diff = 0
-            for f in fish_pos.keys():
-                score_diff += (fish_score[f] / (1 + shortest_distance_squared(hook_pos[0], fish_pos[f]))
-                               - fish_score[f] / (1 + shortest_distance_squared(hook_pos[1], fish_pos[f])))
+            fish_score_diff = sum([fish_score[f] / (1 + shortest_distance_squared(hook_pos[0], fish_pos[f]))
+                                   - fish_score[f] / (1 + shortest_distance_squared(hook_pos[1], fish_pos[f]))
+                                   for f in fish_pos.keys()])
 
-            scores = state.get_player_scores()
-            return score_diff + scores[0] - scores[1]
+            h_value = fish_score_diff + curr_scores[0] - curr_scores[1]
+            heuristic_tree[hashValue] = h_value
+            return [h_value, hashValue]
 
         else:
 
             # move ordering - the move same as previous move first
             children.sort(key=lambda x: (x.move - node.move) ** 2, reverse=False)
 
+            v0 = None
+            if depth == 1:
+                first_child = children[0]
+                try:
+                    v0, prevChildHash = self.alphabeta(ZobristTable, first_child, 0, alpha, beta)
+                    prevChildHash = addHookHash(first_child.state.get_hook_positions(), prevChildHash, ZobristTable)
+                except:
+                    pass
+
             if state.get_player() == 0:
-                v = -1e6
-                for child in children:
-                    v = max(v, self.alphabeta(child, depth - 1, alpha, beta))
+                if v0:
+                    v = v0
+                else:
+                    v = -1e6
+                for child in children[1:]:
+                    if depth == 1:
+                        v = max(v, (self.alphabeta(ZobristTable, child, depth - 1, alpha, beta, prevChildHash))[0])
+                    else:
+                        v = max(v, self.alphabeta(ZobristTable, child, depth - 1, alpha, beta))
                     alpha = max(alpha, v)
                     if beta <= alpha:
                         break
             else:
-                v = 1e6
-                for child in children:
-                    v = min(v, self.alphabeta(child, depth - 1, alpha, beta))
+                if v0:
+                    v = v0
+                else:
+                    v = 1e6
+                for child in children[1:]:
+                    if depth == 1:
+                        v = min(v, (self.alphabeta(ZobristTable, child, depth - 1, alpha, beta, prevChildHash))[0])
+                    else:
+                        v = min(v, self.alphabeta(ZobristTable, child, depth - 1, alpha, beta))
                     beta = min(beta, v)
                     if beta <= alpha:
                         break
@@ -140,9 +194,10 @@ class PlayerControllerMinimax(PlayerController):
         child_nodes = initial_tree_node.compute_and_get_children()
         child_v = []
         global start_time
+        global heuristic_tree
+        heuristic_tree = {}
         for child in child_nodes:
             start_time = time.time()
-            child_v.append(self.alphabeta(child, 4, -1e6, 1e6))
-
+            child_v.append(self.alphabeta(model, child, 2, -1e6, 1e6))
         bestMove = (child_nodes[child_v.index(max(child_v))]).move
         return ACTION_TO_STR[bestMove]
